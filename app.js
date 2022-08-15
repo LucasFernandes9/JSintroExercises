7939bd6c2857bfaad4e35ab53aa27a1991ba8702
LucasFernandes9 <lucascfernandes9@gmail.com>
2022-08-15 00:03:57 -0700

    js intro

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log("Hello World!\n==========\n");
+console.log(
+    "follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+const firstName = "Lucas";
+let lastName = "Fernandes";
+var age = 28;
+
+lastName = "Coble";
+age = 29;
+
+let language = "JavaScript";
+let createdYear = 1995;
+let inCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}`);
+
+let product = "T-shirt"
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectSize = "M";
+
+console.log(`${product} in size ${selectSize} is $${price} with ${stock} in stock`);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} from ${author} is ${pageCount} pages long. You last bookmark was page ${bookmark}.`);
